4c20723e257c4fd8cdf931b9277a786eed48bd76
LeoLChalot <larouchalotleo@gmail.com>
2023-02-08 22:57:03 +0100

    DISPLAY | Modif Media Queries + Animations

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,8 @@ let btnBurger = document.getElementById('btn-burger');
 let asideContainer = document.querySelector('#aside-container');
 let btnBurgerSpan = document.querySelectorAll('#btn-burger span');
 let copyright = document.querySelector('.copyright');
+let body = document.querySelector('body');
+console.log(body.style);
 
 console.log(btnBurger);
 
