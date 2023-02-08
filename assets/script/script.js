2bd815d2d975d513c6a517f2848313cf3798a0c8
LeoLChalot <larouchalotleo@gmail.com>
2023-02-08 12:54:01 +0100

    DISPLAY | Animation Copyright | OK

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
 let btnBurger = document.getElementById('btn-burger');
 let asideContainer = document.querySelector('#aside-container');
 let btnBurgerSpan = document.querySelectorAll('#btn-burger span');
-let copyright = document.querySelectorAll('.copyright');
+let copyright = document.querySelector('.copyright');
 
 console.log(btnBurger);
 
@@ -9,4 +9,5 @@ btnBurger.addEventListener("click", () =>{
   btnBurger.classList.toggle("active");
   asideContainer.classList.toggle("active");
   copyright.classList.toggle("active");
+  console.log(copyright);
 });
